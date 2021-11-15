fca63ef8be246b088586829da7dfe29ce2622f34
Oleg Slyusarov <o.slusarov@gmail.com>
2021-11-15 10:00:53 +0200

    adds basic routes, controllers and repository functions
    adds basic routes, controllers and repository functions for transactions and categories.

---
+++ controllers/transactions.js
@@ -0,0 +1,104 @@
+const Transactions = require('../repository/transactions');
+const Categories = require('../repository/categories');
+
+const { HttpCode, ResponseStatus } = require('../config/constants');
+const { CustomError } = require('../helpers/customError');
+
+const getTransactions = async (req, res) => {
+  const userId = req?.user?._id; // TODO: replace ?.
+  const data = await Transactions.listTransactions(userId, req.query);
+
+  res.status(HttpCode.OK).json({
+    status: ResponseStatus.SUCCESS,
+    code: HttpCode.OK,
+    data: { ...data },
+  });
+};
+
+const saveTransaction = async (req, res) => {
+  const userId = req.user._id;
+  const balanceAfter = 10000.0; // TODO: add calculation of balance after transaction
+
+  const transaction = await Transactions.addTransaction({
+    ...req.body,
+    balanceAfter,
+    owner: userId,
+  });
+
+  res.status(HttpCode.CREATED).json({
+    status: ResponseStatus.SUCCESS,
+    code: HttpCode.CREATED,
+    data: { transaction },
+  });
+};
+
+const removeTransaction = async (req, res) => {
+  const userId = req.user._id;
+  const transaction = await Transactions.removeTransaction(
+    req.params.transactionId,
+    userId,
+  );
+
+  if (transaction) {
+    return res.status(HttpCode.OK).json({
+      status: ResponseStatus.SUCCESS,
+      code: HttpCode.OK,
+      message: 'Deleted',
+      data: { transaction },
+    });
+  }
+
+  throw new CustomError(HttpCode.NOT_FOUND, 'Not found');
+};
+
+const updateTransaction = async (req, res) => {
+  const userId = req.user._id;
+  const transaction = await Transactions.updateTransaction(
+    req.params.transactionId,
+    req.body,
+    userId,
+  );
+
+  if (transaction) {
+    return res.status(HttpCode.OK).json({
+      status: ResponseStatus.SUCCESS,
+      code: HttpCode.OK,
+      data: { transaction },
+    });
+  }
+  throw new CustomError(HttpCode.NOT_FOUND, 'Not found');
+};
+
+const getTransactionStats = async (req, res) => {
+  const userId = req?.user?._id; // TODO: replace ?.
+  const { month, year } = req.query;
+
+  const allCategories = await (
+    await Categories.listCategories()
+  ).map(({ name }) => name);
+  const userStats = await Transactions.listTransactionStats(
+    userId,
+    month,
+    year,
+  );
+
+  // TODO: process month and year parameters here
+  const finalStats = allCategories.reduce(
+    (acc, category) => ({ ...acc, [category]: userStats[category] || 0 }),
+    {},
+  );
+
+  res.status(HttpCode.OK).json({
+    status: ResponseStatus.SUCCESS,
+    code: HttpCode.OK,
+    data: finalStats,
+  });
+};
+
+module.exports = {
+  getTransactions,
+  saveTransaction,
+  removeTransaction,
+  updateTransaction,
+  getTransactionStats,
+};
