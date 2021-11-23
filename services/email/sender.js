a03778cf90a47e95c6ec35668fce4d200764d5a8
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-11-23 23:29:28 +0200

    update email

--- services/email/sender.js
+++ services/email/sender.js
@@ -5,9 +5,7 @@ require('dotenv').config();
 class CreateSenderNodemailer {
   async send(msg) {
     const config = {
-      host: 'smtp.meta.ua',
-      port: 465,
-      secure: true,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,
